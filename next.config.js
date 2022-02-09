d66b33cb4176e727326a68ecbc4f744c2f2dc862
Andre Kristanto <akristan001@e.ntu.edu.sg>
2022-02-09 19:35:13 +0800

    update config and npm script for heroku

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const config = {
 };
 
 // eslint-disable-next-line import/no-extraneous-dependencies
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//   enabled: process.env.ANALYZE === "true",
+// });
 
-module.exports = withBundleAnalyzer(config);
+module.exports = config;
